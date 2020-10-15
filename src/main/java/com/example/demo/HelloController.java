03eea4afc7b0e816db56623da857f1cca51b377d
Hugocandraditya <38039037+Hugocandraditya@users.noreply.github.com>
2020-10-15 08:27:23 +0700

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController{
 	@GetMapping("/get")
 	public String getHello() {
-		return "Hellow";
+		return "Yow";
 	}
 }
